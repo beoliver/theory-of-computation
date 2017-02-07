d22ac75b021416683e1a20e0b00385878b836f47
Benjamin Edward Oliver <benjamin.e.oliver@gmail.com>
2017-02-07 14:08:00 +0100

    Create fa_types.py

---
+++ fa_types.py
@@ -0,0 +1,3 @@
+DFA = namedtuple('DFA', ['Q', 'Sigma', 'delta', 'q0', 'F'])
+NFA = namedtuple('NFA', ['Q', 'Sigma', 'delta', 'q0', 'F'])
+FA  = namedtuple('FA', ['Q', 'Sigma', 'delta', 'q0', 'F', 'type'])
